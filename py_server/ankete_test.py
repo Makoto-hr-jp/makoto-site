279ddaaf15f37b9cfc8e793584c9c3bff3bad029
Tomislav Mamić <tomislav.mamic1@gmail.com>
2018-12-26 14:02:56 +0100

    testovi

---
+++ py_server/ankete_test.py
@@ -0,0 +1,26 @@
+'''
+Autor: TM
+
+Svrha: testiranje API-ja
+
+Preduvjeti: - flask server na 127.0.0.1:5000
+            - postavljena MySQL baza
+'''
+# built-in
+import requests
+import json
+from unittest import TestCase,main
+makej=lambda d: json.dumps(d)
+
+root="http://127.0.0.1:5000/"
+
+def get_status():
+    r=requests.get(url=root+'api-status',json=makej({'a':'b'}))
+    print(r.text)
+    return r.status_code
+
+class osnovne_funkcije(TestCase):
+    def test_status(self):
+        self.assertEqual(get_status(),200)
+
+main()
